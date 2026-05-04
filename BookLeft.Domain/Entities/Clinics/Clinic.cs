67234203d72407b952b8f85d3787b51a9bd9d711
Nickaphy <nickdahchris@gmail.com>
2026-05-04 13:16:44 +0200

    Domain logic for clinic and Openinghours valueobject created and mostly finished for now.

--- BookLeft.Domain/Entities/Clinics/Clinic.cs
+++ BookLeft.Domain/Entities/Clinics/Clinic.cs
@@ -1,5 +1,36 @@
-﻿// Represents a clinic location
+﻿namespace BookRight.Domain.Entities.Clinics;
 
-// Rules:
-// - Has limited room capacity
-// - Limits number of simultaneous bookings
+public class Clinic
+{
+    public Guid ClinicId { get; private set; }
+    public string Name { get; private set; }
+    public string Address { get; private set; }
+    public int AmountTreatmentRooms { get; private set; }
+
+    public List<ClinicOpeningHour> OpeningHours { get; private set; }
+
+    public int MaxSimultaneousBookings => AmountTreatmentRooms;
+
+    private Clinic() { }
+
+    public Clinic(
+        string name,
+        string address,
+        int amountTreatmentRooms)
+    {
+        if (amountTreatmentRooms <= 0)
+            throw new ArgumentException("A clinic must have at least one treatment room.");
+        
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Clinic must have a name.");
+
+        if (string.IsNullOrWhiteSpace(address))
+            throw new ArgumentException("Clinic must have an address.");
+
+        ClinicId = Guid.NewGuid();
+        Name = name;
+        Address = address;
+        AmountTreatmentRooms = amountTreatmentRooms;
+        OpeningHours = new List<ClinicOpeningHour>();
+    }
+}
