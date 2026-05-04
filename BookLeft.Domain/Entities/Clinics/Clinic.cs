fae8c684b2b7cf1e65e0b3361d8036ad33d8d116
Nickaphy <nickdahchris@gmail.com>
2026-05-04 14:02:35 +0200

    Fixed clinic and customer entity, along with creation of the entity aggregate

--- BookLeft.Domain/Entities/Clinics/Clinic.cs
+++ BookLeft.Domain/Entities/Clinics/Clinic.cs
@@ -1,10 +1,13 @@
-﻿namespace BookRight.Domain.Entities.Clinics;
+﻿using BookRight.Domain.Common;
 
-public class Clinic
+namespace BookRight.Domain.Entities.Clinics;
+
+public class Clinic : Entity
 {
-    public Guid ClinicId { get; private set; }
     public string Name { get; private set; }
-    public string Address { get; private set; }
+    public string Street { get; private set; }
+    public string City { get; private set; }
+    public string Zipcode { get; private set; }
     public int AmountTreatmentRooms { get; private set; }
 
     public List<ClinicOpeningHour> OpeningHours { get; private set; }
@@ -15,22 +18,35 @@ public class Clinic
 
     public Clinic(
         string name,
-        string address,
-        int amountTreatmentRooms)
+        int amountTreatmentRooms,
+        string street,
+        string city,
+        string zipcode
+        )
     {
         if (amountTreatmentRooms <= 0)
             throw new ArgumentException("A clinic must have at least one treatment room.");
         
         if (string.IsNullOrWhiteSpace(name))
             throw new ArgumentException("Clinic must have a name.");
+        
+        if (zipcode.Length != 4 )
+            throw new ArgumentException("Zipcode must be 4 digits.", nameof(zipcode));
+        
+        if (zipcode.All(char.IsDigit))
+            throw new ArgumentException("Zipcode must not contain only digits.", nameof(zipcode));
+            
+        if (string.IsNullOrWhiteSpace(city))
+            throw new ArgumentException("City cannot be empty.", nameof(city));
 
-        if (string.IsNullOrWhiteSpace(address))
-            throw new ArgumentException("Clinic must have an address.");
+        if (string.IsNullOrWhiteSpace(street))
+            throw new ArgumentException("Street cannot be empty.", nameof(street));
 
-        ClinicId = Guid.NewGuid();
         Name = name;
-        Address = address;
         AmountTreatmentRooms = amountTreatmentRooms;
         OpeningHours = new List<ClinicOpeningHour>();
+        Street = street;
+        City = city;
+        Zipcode = zipcode;
     }
 }
