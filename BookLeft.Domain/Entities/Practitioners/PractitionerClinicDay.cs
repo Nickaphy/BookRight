6da7ff8478cef3c20399beeeced6f2189116e3ac
miltiadousens <miltiadousens@gmail.com>
2026-05-05 13:55:48 +0200

    PractitionerPartlyDone

---
+++ BookLeft.Domain/Entities/Practitioners/PractitionerClinicDay.cs
@@ -0,0 +1,20 @@
+﻿// Links Practitioner to Clinic per day
+using BookRight.Domain.Common;
+
+public class PractitionerClinicDay : Entity
+{
+    public Guid PractitionerId { get; init; }
+    public Guid ClinicId { get; init; }
+    public DateTime Date { get; init; }
+
+    private PractitionerClinicDay() { }
+
+    public PractitionerClinicDay(Guid practitionerId, Guid clinicId, DateTime date)
+    {
+        PractitionerId = practitionerId;
+        ClinicId = clinicId;
+        Date = date.Date; // Ensure only the date part is stored
+    }
+}
+// CRITICAL RULE:
+// A practitioner can only be assigned to ONE clinic per day
