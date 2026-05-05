145166fb5a9953295407bfd21acbed48fdc6dd4e
SJT92 <thorgaard@live.dk>
2026-05-05 09:59:36 +0200

    Basic Campaign Entity

--- BookLeft.Domain/Entities/Campaigns/Campaign.cs
+++ BookLeft.Domain/Entities/Campaigns/Campaign.cs
@@ -3,3 +3,38 @@
 // Rules:
 // - Has start and end date
 // - Applies to selected treatment types
+
+using BookRight.Domain.Common;
+
+namespace Bookright.Domain.Campaigns;
+
+public class Campaign : Entity
+{
+    public string Name { get; private set; }
+    public DateOnly StartDate { get; private set; }
+    public DateOnly EndDate { get; private set; }
+    public int discountPercent { get; private set; }
+
+
+
+    private Campaign() { }
+
+    public Campaign(string name,
+        DateOnly startDate,
+        DateOnly endDate,
+        int discountPercent
+        )
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Name cannot be empty.", nameof(name));
+        if (startDate > endDate)
+            throw new ArgumentException("Start date cannot be after end date.", nameof(startDate));
+        if (discountPercent < 0 || discountPercent > 100)
+            throw new ArgumentException("Discount percent must be between 0 and 100.", nameof(discountPercent));
+
+        Name = name;
+        StartDate = startDate;
+        EndDate = endDate;
+        this.discountPercent = discountPercent;
+    }
+}
