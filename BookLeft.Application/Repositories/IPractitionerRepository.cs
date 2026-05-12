0b1d4f927ea2d495eacb7602d0faa40f4996dcb1
Data-Erik <sendalskoreskole@gmail.com>
2026-05-12 12:56:25 +0200

    Add IPractitionerRepository interface definition

--- BookLeft.Application/Repositories/IPractitionerRepository.cs
+++ BookLeft.Application/Repositories/IPractitionerRepository.cs
@@ -1,4 +1,18 @@
-﻿// Repository interface
+// Repository interface
 // Defines what the Application layer needs from persistence
 // Implemented later in Infrastructure using EF Core
 // Application depends on abstraction, not concrete database code
+
+using BookRight.Domain.Entities.Practitioners;
+
+
+// Eriks work
+
+namespace BookRight.Application.Repositories;
+
+public interface IPractitionerRepository
+{
+    Task<Practitioner?> GetByIdAsync(
+        Guid practitionerId,
+        CancellationToken cancellationToken = default);
+}
