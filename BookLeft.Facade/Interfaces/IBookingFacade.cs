5cc9e71b138bc1f3696605eefb19d8c80dee0789
Data-Erik <sendalskoreskole@gmail.com>
2026-05-12 13:04:04 +0200

    Implement IBookingFacade interface
    Added interface IBookingFacade with CreateBookingAsync method.

--- BookLeft.Facade/Interfaces/IBookingFacade.cs
+++ BookLeft.Facade/Interfaces/IBookingFacade.cs
@@ -1,4 +1,4 @@
-﻿// Facade interface
+// Facade interface
 // This is the contract used by the Blazor UI
 // UI should depend on this interface instead of Application handlers directly
 // Keeps UI simple and separated from business logic
@@ -24,3 +24,18 @@ BookRightDbContext
 ↓
 SQL Server
 */
+
+// Erik´s work.
+
+using BookRight.Facade.Dtos;
+
+namespace BookRight.Facade.Interfaces;
+
+public interface IBookingFacade
+{
+    Task<Guid> CreateBookingAsync(
+        CreateBookingRequest request,
+        CancellationToken cancellationToken = default);
+}
+
+// UI calls IBookingFacade -> Facade tranlates request into a command -> Application executes the Use Case.
