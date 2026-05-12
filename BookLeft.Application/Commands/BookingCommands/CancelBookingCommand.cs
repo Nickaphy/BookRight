ae07f31931b07bba2d0e5325db2371891059b719
moozsen <moozsen@hotmail.com>
2026-05-12 11:50:50 +0200

    CancelBooking

--- BookLeft.Application/Commands/BookingCommands/CancelBookingCommand.cs
+++ BookLeft.Application/Commands/BookingCommands/CancelBookingCommand.cs
@@ -1,1 +1,19 @@
-﻿
+﻿//11.05.2026
+//Lasse
+//Denne record repræsenterer en kommando for at annullere en booking.
+//Den indeholder kun en egenskab, BookingId, som er nødvendig for at identificere den booking, der skal annulleres.
+//Denne record bruges som input til CancelBookingUseCase, som håndterer logikken for at annullere bookingen.
+
+using BookRight.Domain.Common;
+using System.Runtime.CompilerServices;
+
+public record CancelBookingCommand
+{
+    public Guid BookingId { get; }
+
+    public CancelBookingCommand(Guid bookingId)
+    {
+        BookingId = bookingId;
+    }
+
+}
