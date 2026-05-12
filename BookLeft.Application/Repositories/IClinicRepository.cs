7c114762b661a68bd68e863e16a87c3f2f365440
Data-Erik <sendalskoreskole@gmail.com>
2026-05-12 12:50:16 +0200

    Refactor IClinicRepository with namespace and method
    Added namespace and updated interface definition for IClinicRepository.

--- BookLeft.Application/Repositories/IClinicRepository.cs
+++ BookLeft.Application/Repositories/IClinicRepository.cs
@@ -1,4 +1,18 @@
-﻿// Repository interface
+// Repository interface
 // Defines what the Application layer needs from persistence
 // Implemented later in Infrastructure using EF Core
 // Application depends on abstraction, not concrete database code
+
+using BookRight.Domain.Entities.Clinics;
+
+
+// Eriks work
+
+namespace BookRight.Application.Repositories;
+
+public interface IClinicRepository
+{
+    Task<Clinic?> GetByIdAsync(
+        Guid clinicId,
+        CancellationToken cancellationToken = default);
+}
