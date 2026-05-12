e2c8ba8eedd598d86dcf18e9da82f4321e73c700
Data-Erik <sendalskoreskole@gmail.com>
2026-05-12 12:52:59 +0200

    Update ICustomerRepository.cs

--- BookLeft.Application/Repositories/ICustomerRepository.cs
+++ BookLeft.Application/Repositories/ICustomerRepository.cs
@@ -1,16 +1,18 @@
-﻿// Repository interface
+// Repository interface
 // Defines what the Application layer needs from persistence
 // Implemented later in Infrastructure using EF Core
 // Application depends on abstraction, not concrete database code
-using System.Threading.Tasks;
-using BookRight.Domain.Customers;
 
-namespace BookRight.Application.Repositories
+using BookRight.Domain.Entities.Customers;
+
+
+// Eriks work
+
+namespace BookRight.Application.Repositories;
+
+public interface ICustomerRepository
 {
-    public interface ICustomerRepository
-    {
-        Task AddCustomerAsync(Customer customer);
-        Task<Customer?> GetCustomerByIdAsync(Guid id);
-        Task<Customer?> GetCustomerByPhoneNumberAsync(string phoneNumber);
-    }
+    Task<Customer?> GetByIdAsync(
+        Guid customerId,
+        CancellationToken cancellationToken = default);
 }
