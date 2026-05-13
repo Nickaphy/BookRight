7a0fe9c36422ee1f5e207ef1cd0e3f6914dc5d23
Nicklas Christoffersen <93937135+Nickaphy@users.noreply.github.com>
2026-05-13 10:48:39 +0200

    Update TreatmentType.cs
    Fixed decimal BasePrice to Money BasePrice.
    And changed logic to nullcheck for baseprice

--- BookLeft.Domain/Entities/Treatments/TreatmentType.cs
+++ BookLeft.Domain/Entities/Treatments/TreatmentType.cs
@@ -14,7 +14,7 @@ public class TreatmentType : AggregateRoot
 {
     public string Name { get; private set; }
     public int Duration { get; private set; } // in minutes
-    public decimal BasePrice { get; private set; }
+    public Money BasePrice { get; private set; }
     public AuthorizationType NeedsAuthorisation { get; private set; } //Lucas ændret 09/05
     public int MaxParticipants { get; private set; }
 
@@ -24,7 +24,7 @@ public class TreatmentType : AggregateRoot
     public TreatmentType(
         string name,
         int duration,
-        decimal basePrice,
+        Money basePrice,
         AuthorizationType needsAuthorisation, //Lucas ændret 09/05
         int maxParticipants
         )
@@ -33,9 +33,10 @@ public class TreatmentType : AggregateRoot
             throw new ArgumentException( "Must choose a treatmenttype" );
         if (duration <= 0)
             throw new ArgumentException("Duration must be greater than 0.", nameof(duration));
-        if (basePrice < 0)
-            throw new ArgumentException("Base price cannot be negative.", nameof(basePrice));
-        if (maxParticipants <= 1)
+            
+       if (string.IsNullOrWhiteSpace(name))
+       
+        if (maxParticipants <= 0)
             throw new ArgumentException("Max participants must be greater than 1.", nameof(maxParticipants));
 
         Name = name;
