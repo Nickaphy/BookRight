0855f629b4935fb6edff110b283248d9eaa8cbbc
miltiadousens <miltiadousens@gmail.com>
2026-05-17 13:05:08 +0200

    Nye Ændringer

---
+++ BookLeft.Application/UseCases/ClinicUseCases/UpdateClinicHandler.cs
@@ -0,0 +1,39 @@
+﻿using BookRight.Application.Repositories;
+using BookRight.Domain.Entities.Practitioners;
+using BookRight.Facade.Commands.Clinic;
+using BookRight.Facade.Dtos.ClinicCommand;
+using BookRight.Application.UseCaseExceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Application.UseCases.ClinicUseCases
+{
+    public class UpdateClinicHandler : IUpdateClinic
+    {
+
+        public readonly IClinicRepository _clinicRepository;
+
+        public UpdateClinicHandler(IClinicRepository clinicRepository)
+        {
+            _clinicRepository = clinicRepository;
+        }
+
+        public Task HandleAsync(UpdateClinicRequest request, CancellationToken cancellationToken = default)
+        {
+            var clinic = _clinicRepository.GetByIdAsync(request.Id, cancellationToken).Result;
+            if (clinic == null)
+            {
+                throw new UseCaseException("Clinic not found");
+            }
+
+            // Update clinic properties
+            clinic.UpdateClinic(request.Name, request.AmountTreatmentRooms, request.City, request.Zipcode,request.Street);
+            
+
+            
+
+            return _clinicRepository.UpdateAsync(clinic, cancellationToken);
+        }
+    }
+}
