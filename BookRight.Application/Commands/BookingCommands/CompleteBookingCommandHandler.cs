786e934c0da31af45e4e21139e2c0b4a347269cf
miltiadousens <miltiadousens@gmail.com>
2026-05-22 10:09:24 +0200

    Nye States i Booking

---
+++ BookRight.Application/Commands/BookingCommands/CompleteBookingCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using BookRight.Application.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BookRight.Facade.Commands.Booking;
+using BookRight.Facade.Dtos.BookingCommand;
+
+namespace BookRight.Application.Commands.BookingCommands
+{
+    public class CompleteBookingCommandHandler : ICompleteBookingUseCase
+    {
+        private readonly IBookingRepository _bookingRepository;
+
+        public CompleteBookingCommandHandler(IBookingRepository bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+
+        public async Task ExecuteAsync(CompleteBookingRequest request)
+        {
+            var booking = await _bookingRepository.GetByIdAsync(request.BookingId);
+            if (booking == null)
+                throw new InvalidOperationException("Booking not found.");
+
+            booking.Complete();
+
+            await _bookingRepository.SaveChangesAsync();
+        }
+    }
+}
