0a13cdec1c2ee56ae3c830f9c579bb3d39320a74
Data-Erik <sendalskoreskole@gmail.com>
2026-05-26 12:12:30 +0200

    TestCorrections

--- BookRight.Application.Tests/Pricing/LoyaltyDiscountStrategyTests.cs
+++ BookRight.Application.Tests/Pricing/LoyaltyDiscountStrategyTests.cs
@@ -124,8 +124,8 @@ public class LoyaltyDiscountStrategyTests
             clinicId: Guid.NewGuid(),
             treatmentTypeId: Guid.NewGuid(),
             timeRange: new TimeRange(
-                DateTime.Today.AddHours(10),
-                DateTime.Today.AddHours(11)),
+                DateTime.Today.AddDays(1).AddHours(10),
+                DateTime.Today.AddDays(1).AddHours(11)),
             basePrice: new Money(basePrice));
 
         var customer = Customer.Create(
